3fd4f74d91abf2a27f79812ee4b147f328449100
MatteoBollecchino <116268391+MatteoBollecchino@users.noreply.github.com>
2024-08-06 17:15:21 +0200

    Add files via upload

---
+++ funzioniClient.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "funzioniClient.h"
+
+void visualizzazioneRubrica(){
+
+}
+
+void ricercaRecordCognome(char cognome[]){
+
+}
+
+void ricercaRecordNomeCognome(char nome[], char cognome[]){
+
+}
+
+void aggiungiRecord(recordRub record){
+
+}
+
+void rimuoviRecord(recordRub record){
+
+}
+
+void modificaTelefono(char vecchioTelefono[], char nuovoTelefono[]){
+
+}
+
+void modificaIndirizzo(char vecchioIndirizzo[], char nuovoIndirizzo[]){
+    
+}
