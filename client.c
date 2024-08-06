fc342422ce9442ba5f4bd15305c45038e398a886
MatteoBollecchino <116268391+MatteoBollecchino@users.noreply.github.com>
2024-08-06 17:17:31 +0200

    Update client.c

--- client.c
+++ client.c
@@ -47,29 +47,19 @@ int main(int argc, char *argv[])
 
     // Fase in cui il client determina l'operazione da richiedere la server
 
-    int richiesta = 0;
-    printf("Menù delle operazione che possono essere richieste dal client: \n");
-    printf("1) Visualizzazzione tutti i record della rubrica \n");
-    printf("2) Ricerca record tramite cognome \n");
-    printf("3) Ricerac record tramite coppia nome-cognome \n");
-    printf("4) Aggiunta Record \n");
-    printf("5) Eliminazione Record \n");
-    printf("6) Modifica Numero di Telefono \n");
-    printf("7) Modifica Indirizzo \n");
-
-    scanf("Inserire numero operazione richiesta: %d", &richiesta);
-    if (richiesta < 1 || richiesta > 7)
-    {
+    int richiesta = atoi(argv[1]);
+    if(richiesta < 1 || richiesta >7){
         generazioneErrore("Codice richiesta non valido \n");
     }
 
     // Fase in cui il client inoltra, tramite socket, il codice della richiesta al server
 
-    int scritto = send(clientFd, &richiesta, sizeof(richiesta), 0);
-    if (scritto < 0)
-    {
+    int scritto = send(clientFd,&richiesta,sizeof(richiesta),0);
+    if(scritto < 0){
         generazioneErrore("Scrittura su socket fallita \n");
     }
+
+    // Fase in cui il client riceve l'eventuale richiesta di password
 
     // Chiusura della connessione con il Server
     close(clientFd);
